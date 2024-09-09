7fe3df7aaa8018cbaece619bdc0a73404ee1f947
Amber Liu <amber.m.liu@hotmail.com>
2024-09-09 16:33:12 -0400

    Add Reduce class.

--- src/Reduce.java
+++ src/Reduce.java
@@ -1,4 +1,20 @@
-package PACKAGE_NAME;
-
 public class Reduce {
+    public static void main(String[] args) {
+        int step = 0;
+        int num = 100;
+
+        while (num > 0) {
+            // If the number is even, divide the number by 2.
+            if (num % 2 == 0) {
+                num /= 2;
+            }
+            else {
+                num -= 1;
+            }
+
+            step++;
+        }
+
+        System.out.println(step);
+    }
 }
